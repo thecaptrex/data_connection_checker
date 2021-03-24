5339d5a8b9a426227a292d954161d26dfb7b916a
thecaptrex <60184003+thecaptrex@users.noreply.github.com>
2021-03-24 13:34:21 +0700

    Update data_connection_checker.dart

--- lib/data_connection_checker.dart
+++ lib/data_connection_checker.dart
@@ -57,7 +57,7 @@ class DataConnectionChecker {
   /// | 208.67.222.222 | OpenDNS    | https://use.opendns.com/                        |
   /// | 208.67.220.220 | OpenDNS    | https://use.opendns.com/                        |
   static final List<AddressCheckOptions> DEFAULT_ADDRESSES = List.unmodifiable([
-  InternetAddress('example.com', type; InternetAddressType.IPv4),
+  InternetAddress('example.com'),
       port: DEFAULT_PORT,
       timeout: DEFAULT_TIMEOUT,
     ),
